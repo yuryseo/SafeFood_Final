c0f82b13f8babf2e9430e3ac27a8f54e074cc515
LSS <cjswosuny01@naver.com>
2019-11-22 10:46:06 +0900

    commit test

---
+++ src/main/java/com/ssafy/SafeFoodFinalSeoul06SyrLssApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeFoodFinalSeoul06SyrLssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeFoodFinalSeoul06SyrLssApplication.class, args);
+	}
+
+}
